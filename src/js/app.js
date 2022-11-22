33df11e28bdaab7888aad76f6c0db78732f8180f
unknown <grosheff.ivan@gmail.com>
2022-11-22 20:58:29 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import "./modules/regularExpressions.js";
+import "./modules/popup.js";
+// import "./modules/burger.js";
+// import "./modules/swiper.js";
+// import "./modules/ScrollTo.js";
+// import "./modules/geolocation.js";
+// import "./modules/accordion.js";
+// import "./modules/date.js";
