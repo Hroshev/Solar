eda9ad775da28109227bcfe0148f1f8a6d367b95
Ivan Hroshev <grosheff.ivan@gmail.com>
2022-12-01 21:34:58 +0200

    first commit

--- src/js/modules/regularExpressions.js
+++
@@ -1,9 +0,0 @@
-//input only number
-const onlyNumber = document.querySelectorAll("#get-tel");
-for (let i = 0; i < onlyNumber.length; i++) {
-  const number = onlyNumber[i];
-
-  number.oninput = function () {
-      this.value = this.value.replace(/\D/g, "").substr(0, 13);
-  };
-}
