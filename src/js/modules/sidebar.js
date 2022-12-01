883b647d88af8cfdeb755d7780a48ade3e87a8a4
Ivan Hroshev <grosheff.ivan@gmail.com>
2022-12-01 21:31:41 +0200

    finish work

--- src/js/modules/sidebar.js
+++ src/js/modules/sidebar.js
@@ -1,4 +1,7 @@
-const container = document.querySelector('.benefits__sidebar-list')
+const container = document.querySelector('.benefits__sidebar-list');
+const saleContent = document.querySelector('.benefits__sidebar-sale');
+const installationContent = document.querySelector('.benefits__sidebar-installation');
+const restrictionsContent = document.querySelector('.benefits__sidebar-restrictions');
 
 container.addEventListener('click', function(e) {
     e.preventDefault();
@@ -9,5 +12,19 @@ container.addEventListener('click', function(e) {
   	item.classList.remove('benefits__sidebar--active')
   })
   target.classList.add('benefits__sidebar--active')
-
+  if(target.classList.contains('sale')){
+    saleContent.style.display = "block";
+    installationContent.style.display = "none";
+    restrictionsContent.style.display = "none";
+  }
+  else if(target.classList.contains('installation')){
+    installationContent.style.display = "block";
+    saleContent.style.display = "none";
+    restrictionsContent.style.display = "none";
+  }
+  else if(target.classList.contains('restrictions')){
+    restrictionsContent.style.display = "block";
+    saleContent.style.display = "none";
+    installationContent.style.display = "none";
+  }
 })
